a88a82d927846c516f9036e104f1e47a11f64909
Hiroki.T <hirodora@me.com>
2024-11-10 16:47:03 +0900

    update layout

--- .quartz_config/quartz.layout.ts
+++ .quartz_config/quartz.layout.ts
@@ -26,7 +26,11 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.RecentNotes({
+      limit: 5,
+      linkToMore: true,
+    }),
   ],
   right: [
     Component.Graph({
