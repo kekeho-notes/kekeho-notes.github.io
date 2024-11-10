3d767e10a7f365784df8123843eedc511714c40e
Hiroki.T <hirodora@me.com>
2024-11-10 16:35:58 +0900

    github workflow

--- .quartz_config/quartz.layout.ts
+++ .quartz_config/quartz.layout.ts
@@ -29,7 +29,14 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+      localGraph: {
+        depth: 3,
+      },
+      globalGraph: {
+        depth: -1,
+      },
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
